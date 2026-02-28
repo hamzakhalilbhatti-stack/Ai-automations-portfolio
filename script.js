2694257c8d8c7df7791609436e84962f91cbd622
hamzakhalilbhatti-stack <hamzakhalilbhattibrothers@gmail.com>
2026-02-28 23:37:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -98,6 +98,6 @@ window.addEventListener("load", function () {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-  });
+  }
 
 });
