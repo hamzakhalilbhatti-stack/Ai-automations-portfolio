fd85c1fb598491b6f07848312b06cc26445e4d48
hamzakhalilbhatti-stack <hamzakhalilbhattibrothers@gmail.com>
2026-03-03 00:30:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -263,3 +263,36 @@ revealElements.forEach(el => {
     el.classList.add("hidden");
     revealObserver.observe(el);
 });
+/* ================= COUNTER ANIMATION ================= */
+
+const counters = document.querySelectorAll('.counter');
+
+const counterObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            const counter = entry.target;
+            const target = +counter.getAttribute('data-target');
+            let count = 0;
+
+            const updateCounter = () => {
+                const increment = target / 80;
+                count += increment;
+
+                if (count < target) {
+                    counter.innerText = Math.floor(count);
+                    requestAnimationFrame(updateCounter);
+                } else {
+                    counter.innerText = target;
+                }
+            };
+
+            updateCounter();
+            counterObserver.unobserve(counter);
+        }
+    });
+}, { threshold: 0.6 });
+
+counters.forEach(counter => {
+    counterObserver.observe(counter);
+});
